b41fa07e57d4a231ebd7422325656cf8658b6cc6
abxxbo <devavcado@gmail.com>
2022-10-02 11:48:26 -0500

    read program into buffer

--- src/rraa-asm.c
+++ src/rraa-asm.c
@@ -8,6 +8,13 @@ short memory[STACK_END];
 short ip = 0;
 
 
+// Read program into memory to assemble
+uint8_t* read_program(char* file){
+  uint8_t* buffer = 0;
+  FILE* fp = fopen(file, "rb");
+  fread(buffer, 1024, 1, fp);
+  return buffer;
+}
 
 int main(){
   return 0;
