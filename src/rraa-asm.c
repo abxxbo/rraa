eee218b1b26cc3862766d470a87c435f99271a72
abxxbo <devavcado@gmail.com>
2022-10-02 13:34:49 -0500

    file is in buffer

--- src/rraa-asm.c
+++ src/rraa-asm.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include "asm.h"
 
@@ -8,14 +10,24 @@ short memory[STACK_END];
 short ip = 0;
 
 
-// Read program into memory to assemble
-uint8_t* read_program(char* file){
-  uint8_t* buffer = 0;
+int main(int argc, char** argv){
+  if(argc < 2){
+    fprintf(stderr,"usage: rasm  [file] [--no-krnl]");
+    return 2;
+  }
+
+  char* file = argv[1];
+  bool no_krnl = false; // assume false
+  char* output_file = "a.out";  
+  
+  for(int i = 0; i < argc; i++)
+    if(strcmp(argv[i], "--no-krnl") == 0) no_krnl = true;
+
+  uint8_t buffer[1024];
   FILE* fp = fopen(file, "rb");
   fread(buffer, 1024, 1, fp);
-  return buffer;
-}
 
-int main(){
+  // File is in buffer, read.
+
   return 0;
 }
