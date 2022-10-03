29c00c67b5fa542d1b3f61417c6f1d3b43d51d25
abxxbo <devavcado@gmail.com>
2022-10-02 19:59:01 -0500

    arguments

--- src/rraa-emu.py
+++ src/rraa-emu.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 
 from sys import argv, exit
+
+bin = ""
+
+# Check all arguments for binary file.
+def show_help():
+	print("Usage: rraa-emu.py [--bin] [rraa binary]")
+	exit(2)
+
+for i in range(len(argv)):
+	if argv[i] == "--help":
+		show_help()
+	if argv[i] == "--bin":
+		bin = argv[i+1]
+	else:
+		continue
